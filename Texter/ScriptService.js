b336fb721638ccb19f86bb64eee778be41054a29
Ammon Morris <likesalmon@gmail.com>
2015-10-17 07:14:00 -0700

    refactored script service to use api

--- Texter/ScriptService.js
+++ Texter/ScriptService.js
@@ -5,100 +5,23 @@ module.exports = [
     '$resource',
     '$window',
     function (API, $resource, $window) {
-        // var apiUrl = API.protocol + '://localhost:' + API.port;
-        //
-        // if (!/localhost/.test($window.location.href)) {
-        //     apiUrl = API.protocol + '://' + API.ip + ':' + API.port;
-        // }
+        var apiUrl = '';
+        
+        if (/localhost/.test($window.location.href)) {
+            apiUrl = API.protocol + '://localhost:' + API.port;
+        } else {
+            apiUrl = API.protocol + '://' + API.ip + ':' + API.port;
+        }
 
-        var scripts = [
+        return $resource(apiUrl + '/api/scripts/:id',
             {
-                id: 1,
-                title: 'This is a title',
-                content: 'This is a script'
+                id: '@id'
+            },
+            {
+                update: {
+                    method: 'PUT'
+                }
             }
-        ];
-
-        var self = this;
-
-        return {
-            $query: function () {
-                return scripts;
-            },
-            $save: function (script) {
-                script.id = scripts.length + 1;
-                scripts.push(script);
-            },
-            $update: function (script) {
-                var oldScript = scripts.filter(function (c) {
-                    return c.id === script.id;
-                });
-
-                oldScript.name = script.title;
-                oldScript.number = script.content;
-
-                return scripts;
-            },
-            $remove: function (script) {
-                scripts = scripts.filter(function (c) {
-                    return c.id !== script.id;
-                });
-
-                return scripts;
-            }
-        };
-
-        // return $resource(apiUrl + '/api/scripts/:id',
-        //     {
-        //         id: '@id'
-        //     },
-        //     {
-        //         update: {
-        //             method: 'PUT'
-        //         }
-        //     }
-        // );
-
-        // return new Script();
-
-        // var scripts = [
-        //     {
-        //         id: 1,
-        //         title: 'First Script',
-        //         content: 'This is the first script. It is long.'
-        //     },
-        //     {
-        //         id: 2,
-        //         title: 'Second Script',
-        //         content: 'This is the second script'
-        //     }
-        // ];
-        //
-        // return {
-        //     get: function () {
-        //         return scripts;
-        //     },
-        //     save: function (script) {
-        //         script.id = scripts.length + 1;
-        //         scripts.push(script);
-        //     },
-        //     update: function (script) {
-        //         var oldContact = scripts.filter(function (c) {
-        //             return c.id === script.id;
-        //         });
-        //
-        //         oldContact.name = script.name;
-        //         oldContact.number = script.number;
-        //
-        //         return scripts;
-        //     },
-        //     remove: function (script) {
-        //         scripts = scripts.filter(function (c) {
-        //             return c.id !== script.id;
-        //         });
-        //
-        //         return scripts;
-        //     }
-        // };
+        );
     }
 ];
